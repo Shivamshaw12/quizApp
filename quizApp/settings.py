a312659c76284d2200c1876edc7fffcea75b6fff
shivam shaw <shivam12shaw@gmail.com>
2022-08-03 22:28:27 +0530

    changes in static

--- quizApp/settings.py
+++ quizApp/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 #     '/var/www/static/',
 # ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
