72daf64e8be0584577d14a381313de0291ffa92f
shivam shaw <shivam12shaw@gmail.com>
2022-08-07 11:54:37 +0530

    c

--- quizApp/settings.py
+++ quizApp/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 #     '/var/www/static/',
 # ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'https://quizzerapp1.herokuapp.com/static/js/script.js'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
